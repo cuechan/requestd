c19c37693d853457ff99135e3e2850d3dfa96646
Paul Maruhn <paulmaruhn@posteo.de>
2020-07-22 20:29:36 +0200

    remove unused tables

--- src/collector/init_db.sql
+++ src/collector/init_db.sql
@@ -6,19 +6,3 @@ CREATE TABLE IF NOT EXISTS nodes (
 	lastaddress TEXT NOT NULL,
 	lastresponse TEXT NOT NULL DEFAULT '{}'
 );
-
-
-CREATE TABLE IF NOT EXISTS trigger (
-	nodeid TEXT PRIMARY KEY,
-	status TEXT,
-	UNIQUE(nodeid, status)
-);
-
-
-CREATE TABLE IF NOT EXISTS responses (
-	nodeid TEXT,
-	timestamp TEXT,
-	category TEXT,
-	data TEXT,
-	UNIQUE(nodeid, timestamp)
-);
